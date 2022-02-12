213e239fd5857945830c0e6c2f50270a239dcfcc
Brian Coutts <bjhcoutts@gmail.com>
2022-02-12 11:39:44 -0800

    currency signs have been added if applicable

--- src/components/shared/coinCard/CoinCard.svelte
+++ src/components/shared/coinCard/CoinCard.svelte
@@ -13,13 +13,20 @@
 	
 	const currencyFormatter = (currency:string, amount: number) => {
 
-		const format = Intl.NumberFormat('en-US', {
+		const formatter = Intl.NumberFormat('en-US', {
+			style: 'currency',
 			currency,
 			minimumFractionDigits: 2,
 			maximumFractionDigits: 2,
 		});
 
-		return format.format(amount)
+		let result = formatter.format(amount)
+
+		if (formatter.format(amount).slice(0,3) === currency) {
+			result = result.substring(3)
+		}
+
+		return result
 		
 	}
 	
